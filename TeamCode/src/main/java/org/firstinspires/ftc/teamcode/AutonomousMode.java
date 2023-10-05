38da475e757bd1c372a719ca749831264b90a08d
ConnorFeeney <connor.w.feeney@gmail.com>
2023-10-04 22:55:22 -0400

    Added camera functionality, Created utils file for shape detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
@@ -1,21 +1,61 @@
 package org.firstinspires.ftc.teamcode;
 
+import org.firstinspires.ftc.teamcode.Utils.*;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @Autonomous(name = "FTC Autonomous Mode (23-24)")
 public class AutonomousMode extends LinearOpMode {
     @Override
     public void runOpMode() {
+        //adds telemetry
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+
+        //Initializes camera
+        initCamera();
+
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
+        if(opModeIsActive()){
+            //adds telemetry
             telemetry.addData("Status", "Running");
             telemetry.update();
         }
+        while (opModeIsActive()) {
+            //Main Code Loop
+        }
+    }
+
+    private void initCamera(){
+        //Creates Camera
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam1");
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                //Starts Stream
+                camera.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+                telemetry.log().add("Camera | Streaming");
+                telemetry.update();
+            }
+            @Override
+            public void onError(int errorCode)
+            {
+                //handles failed camera launch
+                telemetry.log().add("Camera | Failed Streaming");
+                telemetry.update();
+            }
+        });
     }
 }
